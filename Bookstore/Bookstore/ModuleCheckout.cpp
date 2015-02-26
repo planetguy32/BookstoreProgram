c93ba628497ca2150579cb4312cd082c143e1da0
planetguy <planetguy32@users.noreply.github.com>
2015-02-26 14:15:30 -0800

    Rough implementation of checkout module. TODO hook it up to inventory

--- Bookstore/Bookstore/ModuleCheckout.cpp
+++ Bookstore/Bookstore/ModuleCheckout.cpp
@@ -1,4 +1,8 @@
 #include "ModuleCheckout.h"
+#include "ModuleInventory.h"
+
+#include <iostream>
+#include <iomanip>
 
 
 ModuleCheckout::ModuleCheckout()
@@ -12,5 +16,38 @@ ModuleCheckout::~ModuleCheckout()
 
 bool ModuleCheckout::doInteraction()
 {
-	return false;
+	bool moreItems=true;
+	double orderPrice=0;
+	cout << setprecision(2) << fixed;
+	while(moreItems)
+	{
+		string book;
+		cout << "Book title: ";
+		cin >> book;
+		if(book.length  == 0)
+		{
+			moreItems=false;
+			cout << "Subtotal: " << orderPrice;
+			cout << "Total: "
+			cout << "Thank you for your business." << endl;
+		}
+		else
+		{
+			Book * book;
+			//TODO lookup from DB
+			int qty=book->getQty();
+			if(qty <= 1)
+			{
+				//TODO handle book out of stock
+			}
+			else
+			{
+				book->setQty(qty-1);
+			}
+			double price=book->getRetail();
+			cout << book->getTitle() << endl;
+			cout << "   $" << price << endl << endl;
+			orderPrice += price;
+		}
+	}
 }
