1eeea2a2a96ca09d86d37a7df9c6a389230df6aa
trimun <magnawiz@gmail.com>
2015-03-17 14:02:47 -0700

    added report files

--- Bookstore/Bookstore/ModuleReport.h
+++ Bookstore/Bookstore/ModuleReport.h
@@ -1,12 +1,27 @@
+//HEADER FILE:
+
+
 #pragma once
 #include "Module.h"
+#include <vector>
+
 class ModuleReport :
 	public Module
 {
 public:
-	ModuleReport();
+	ModuleInventory *inventory;
+	ModuleReport(ModuleInventory *inventory);
 	~ModuleReport();
 
-	bool doInteraction();
+	bool doInteraction(); 
+
+	void listInv();
+	void wholeValue();
+	void retailValue();
+	void listByQty();
+	void listByCost();
+	void listByAge();
+
+
 };
 
