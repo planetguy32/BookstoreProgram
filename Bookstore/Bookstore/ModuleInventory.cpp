34ce150cc36f4f310960f91d63553244c385c384
rezabayat <bayat_r@yahoo.com>
2015-03-17 02:52:46 -0700

    Update ModuleInventory.cpp

--- Bookstore/Bookstore/ModuleInventory.cpp
+++ Bookstore/Bookstore/ModuleInventory.cpp
@@ -43,6 +43,7 @@ else if don't find it
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <string>
 
 #include "Module.h" 
 #include "ModuleInventory.h"
@@ -80,7 +81,7 @@ ModuleInventory::ModuleInventory()
 
 Book * ModuleInventory::parseBook(std::string * subParts)
 {
-	Book * book=0;
+	Book * book = 0;
 	if (subParts)
 	{
 		book = new Book;
@@ -156,7 +157,6 @@ ModuleInventory::~ModuleInventory()
 
 bool ModuleInventory::doInteraction()
 {
-	std::string * NewBook = new std::string[8];
 	bool moreItems = true;
 	char AddBook;
 
@@ -180,25 +180,19 @@ bool ModuleInventory::doInteraction()
 	{
 		long long int bookISBN;
 		std::cout << "ISBN: ";
-		if (!(std::cin >> bookISBN))
+		if (!(std::cin >> bookISBN)||choice == 5)
 		{
 			moreItems = false;
 		}
 		else
 		{
+			moreItems = false;
 
-			//			Book * book = inventory->getBook(bookISBN);
 			Book * book = getBook(bookISBN);
 			if (book != 0)
 			{
-				std::cout << " ISBN : " << book->getISBN() << std::endl;
-				std::cout << " Title : " << book->getTitle() << std::endl;
-				std::cout << " Author : " << book->getAuthor() << std::endl;
-				std::cout << " Publisher : " << book->getPub() << std::endl;
-				std::cout << " Date Added to Inventory : " << book->getDate() << std::endl;
-				std::cout << " Quantity On Hand : " << book->getQty() << std::endl;
-				std::cout << " Wholesale Cost $: " << book->getWhole() << std::endl;
-				std::cout << " Retail Price $: " << book->getRetail() << std::endl;
+				book = Action(book);
+
 			}
 			else //null book
 			{
@@ -209,6 +203,7 @@ bool ModuleInventory::doInteraction()
 
 				if (AddBook == 'y' || AddBook == 'Y')
 				{
+					std::string * NewBook = new std::string[8];
 					Book * book = new Book;
 					NewBook[0] = bookISBN;
 					std::cout << std::endl << " Title : ";
@@ -221,21 +216,16 @@ bool ModuleInventory::doInteraction()
 					std::cin >> NewBook[4];
 					std::cout << std::endl << " Quantity On Hand : ";
 					std::cin >> NewBook[5];
-					std::cout << std::endl << " Wholesale Cost $: ";
+					std::cout << std::endl << " Wholesale Cost : $";
 					std::cin >> NewBook[6];
-					std::cout << std::endl << " Retail Price $: ";
+					std::cout << std::endl << " Retail Price : $";
 					std::cin >> NewBook[7];
 
-					book->setISBN(bookISBN);
-					book->setTitle(NewBook[1]);
-					book->setAuthor(NewBook[2]);
-					book->setPub(NewBook[3]);
-					book->setDate(NewBook[4]);
-					book->setQty(stoi(NewBook[5]));
-					book->setWhole(stoi(NewBook[6]));
-					book->setRetail(stoi(NewBook[7]));
+					book = parseBook(NewBook);
 
+					books.push_back(book);
 
+					std::sort(books.begin(), books.end(), compare);
 				}
 
 			}
@@ -246,7 +236,7 @@ bool ModuleInventory::doInteraction()
 
 Book * ModuleInventory::getBook(long long int isbn)
 {
-	return findBook(isbn, &Book::getISBN);
+	return getBook(isbn, &Book::getISBN);
 }
 
 template<class T>
@@ -258,14 +248,132 @@ Book * ModuleInventory::getBook(T isbn, T (Book::* f)())
 template<class T>
 Book * ModuleInventory::findBook(T isbn, T (Book::* f)(), int start, int end)
 {
-	int midpt = start+(end - start) / 2;
-	T otherISBN = books[midpt]->*f();
+	return books[getBookIndex(isbn, f, start, end)];
+}
+
+template<class T>
+int ModuleInventory::getBookIndex(T isbn, T(Book::* f)(), int start, int end)
+{
+	int midpt = start + (end - start) / 2;
+	T otherISBN = (books[midpt]->*f)();
 	if (isbn == otherISBN)
-		return books[midpt];
-	if (end <= start && otherISBN != isbn)
-		return 0;
+		return midpt;
+	if (end <= start)
+		return -1;
 	if (isbn > otherISBN)
-		return findBook(isbn, midpt+1, end);
+		return getBookIndex(isbn, f, midpt + 1, end);
 	else
-		return findBook(isbn, start, midpt-1);
+		return getBookIndex(isbn, f, start, midpt - 1);
+}
+
+
+Book * ModuleInventory::Action(Book* book)
+{
+	char AddBook;
+
+	std::string * NewBook = new std::string[8];
+
+	NewBook[0] = std::to_string (book->getISBN());
+	NewBook[1] = book->getTitle();
+	NewBook[2] = book->getAuthor();
+	NewBook[3] = book->getPub();
+	NewBook[4] = book->getDate();
+	NewBook[5] = std::to_string(book->getQty());
+	NewBook[6] = std::to_string(book->getWhole());
+	NewBook[7] = std::to_string(book->getRetail());
+
+
+	std::cout << " ISBN : " << NewBook[0] << std::endl;
+	std::cout << " Title : " << NewBook[1] << std::endl;
+	std::cout << " Author : " << NewBook[2] << std::endl;
+	std::cout << " Publisher : " << NewBook[3] << std::endl;
+	std::cout << " Date Added to Inventory : " << NewBook[4] << std::endl;
+	std::cout << " Quantity On Hand : " << NewBook[5] << std::endl;
+	std::cout << " Wholesale Cost : $" << NewBook[6] << std::endl;
+	std::cout << " Retail Price : $" << NewBook[7] << std::endl;
+
+	std::cout << "What will you do with this information ?" << std::endl;
+	std::cout << "    1. Just was looking for?" << std::endl;
+	std::cout << "    2. Edit" << std::endl;
+	std::cout << "    3. Delete" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << ":";
+	int choice = -1;
+	while (!(std::cin >> choice) || choice > 3 || choice < 0)
+	{
+		std::cout << "Invalid choice - only 1-3 are allowed" << std::endl;
+	}
+	if (choice == 2)
+	{
+		std::cout << "Which information of book are you going to Edit?" << std::endl;
+		std::cout << "    1. Title" << std::endl;
+		std::cout << "    2. Author" << std::endl;
+		std::cout << "    3. Publisher" << std::endl;
+		std::cout << "    4. Date Added to Inventory" << std::endl;
+		std::cout << "    5. Quantity On Hand" << std::endl;
+		std::cout << "    6. Wholesale Cost" << std::endl;
+		std::cout << "    7. Retail Price" << std::endl;
+		std::cout << "    8. Return to main Menu" << std::endl;
+
+		std::cout << std::endl;
+		std::cout << ":";
+		int choice = -1;
+		while (!(std::cin >> choice) || choice > 8 || choice < 0)
+		{
+			std::cout << "Invalid choice - only 1-8 are allowed" << std::endl;
+		}
+
+		switch (choice)
+		{
+		case 1:
+			std::cout << " Title : ";
+			std::cin.ignore();
+			std::getline(std::cin ,NewBook[1]);
+			break;
+		case 2:
+			std::cout << " Author : " ;
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[2]);
+			break;
+		case 3:
+			std::cout << " Publisher : ";
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[3]);
+			break;
+		case 4:
+			std::cout << " Date Added to Inventory : " ;
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[4]);
+			break;
+		case 5:
+			std::cout << " Quantity On Hand : " ;
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[5]);
+			break;
+		case 6:
+			std::cout << " Wholesale Cost : $" ;
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[6]);
+			break;
+		case 7:
+			std::cout << " Retail Price : $" ;
+			std::cin.ignore();
+			std::getline(std::cin, NewBook[7]);
+			break;
+		case 8:
+			break;
+		default:
+			break;
+		}
+		book = parseBook(NewBook);
+		books.push_back(book);
+		std::sort(books.begin(), books.end(), compare);
+
+	}
+	else if (choice == 3)
+	{
+	//	books.erase(book );
+	}
+	return 0;
 }
